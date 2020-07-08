e24d70344951411bc788a6304bd3f6b01f8c043d
MihMarMar <genderlik@gmail.com>
2020-07-08 17:25:54 +0200

    [manarm-gui] fix subscriber shutdown

--- Simulation/GUI/src/gui/main_window.py
+++ Simulation/GUI/src/gui/main_window.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 import Tkinter as Tk
 
 import rospy
@@ -5,20 +7,32 @@ import rospy
 from src.gui.move.move_frame import MoveFrame
 from src.gui.visualisation.robot_frame import RobotFrame
 
-# todo the window doesn't stop properly
-
-rospy.init_node("manarm_gui")
-
 form = Tk.Tk()
-form.title("ManArm controller")
 
-myLabel = Tk.Label(form, text="ManArm controller")
-myLabel.pack()
 
-move_frame = MoveFrame(form)
-move_frame.pack(side=Tk.LEFT)
+def on_close():
+    global form
+    rospy.signal_shutdown("User closed the window")
+    form.destroy()
 
-robot_frame = RobotFrame(form)
-robot_frame.pack(side=Tk.LEFT)
 
-form.mainloop()
+def start_gui():
+    global form
+    rospy.init_node("manarm_gui")
+    form.title("ManArm controller")
+
+    title_label = Tk.Label(form, text="ManArm controller")
+    title_label.pack()
+
+    move_frame = MoveFrame(form)
+    move_frame.pack(side=Tk.LEFT)
+
+    robot_frame = RobotFrame(form)
+    robot_frame.pack(side=Tk.LEFT)
+
+    form.protocol("WM_DELETE_WINDOW", on_close)
+    form.mainloop()
+
+
+if __name__ == '__main__':
+    start_gui()
